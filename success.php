a0c766b509e312e61ff7ed868b38cd7c25256e72
nikirf6 <130364383+nikirf6@users.noreply.github.com>
2023-12-20 09:31:11 +0100

    Create success.php

---
+++ success.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Submitted Successfully</title>
+</head>
+<body>
+    <h1>Form Submitted Successfully!</h1>
+    <p>Thank you for your submission.</p>
+    <p><a href="index.php">Back to the website</a></p>
+</body>
+</html>
