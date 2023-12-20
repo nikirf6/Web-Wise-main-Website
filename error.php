ae1abbbcb847784343aa8eaeedc2d147e0a81cc2
nikirf6 <130364383+nikirf6@users.noreply.github.com>
2023-12-20 09:32:17 +0100

    Create error.php

---
+++ error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error Submitting Form</title>
+</head>
+<body>
+    <h1>Error Submitting Form</h1>
+    <p>There was an error while processing your submission. Please try again.</p>
+    <p><a href="webwise.store">Back to the website</a></p>
+</body>
+</html>
