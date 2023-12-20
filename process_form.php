b47665ec2732a75bea97bc0c859a552640c03fe5
nikirf6 <130364383+nikirf6@users.noreply.github.com>
2023-12-20 08:49:59 +0100

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['submit'])){
+    $first_Name = $_POST['first_name'];
+    $last_Name = $_POST['last_name'];
+    $emailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "webwise@artistrydesign.net";
+    $headers = "From: $emailFrom";
+
+    if (mail($mailTo, "Subject: Contact Form Submission", $message, $headers)) {
+        // Email sent successfully
+        header("Location:success.html");
+        exit();
+    } else {
+        // Email sending failed
+        header("Location: index.php?mailsend=error");
+        exit();
+    }
+}
+?>
